ff1e7c6b96fc806bea9e3d9d66e5e096c1861c14
diegocastelo <diegocastelogla@gmail.com>
2024-09-08 09:35:48 -0300

    :art: refactor: reformatando a main

--- main.py
+++ main.py
@@ -2,33 +2,28 @@ import networkx as nx
 import numpy as np
 import matplotlib.pyplot as plt
 import random
+from consts import dict_final_states
+from helpers import generate_random_initial_state
 
 
-def generate_random_initial_state(square_side_size: int):
-    array = list(range(1, square_side_size ** 2)) + ['b']
-
-    # Gerar a matriz preenchida com 'x'
-    matrix = np.full((square_side_size, square_side_size), 'x', dtype=object)
-
-    count_x = 0
-    count_y = 0
-    while len(array) != 0:
-        random_index = random.randint(0, len(array) - 1)
-        element = array[random_index]
-        array.pop(random_index)
-        matrix[count_x][count_y] = element
-        if count_y == (square_side_size-1):
-            count_x += 1
-            count_y = 0
-        else:
-            count_y += 1
-
-    return matrix
-
-if __name__ == '__main__':
+def main():
     G = nx.Graph()
+    square_side_size = int(input("Digite o tamanho do lado do quadrado: "))
+    if square_side_size in dict_final_states:
+        final_state = dict_final_states.get(square_side_size)
+    else:
+        return print('Cadastre o estado final desejado no arquivo consts')
 
-    initial_state = np.array(generate_random_initial_state(5))
+    initial_state = np.array(generate_random_initial_state(square_side_size))
 
     G.add_node(1, matriz=initial_state)
 
+    return True
+
+
+if __name__ == '__main__':
+    try:
+        main()
+    except Exception as e:
+        print(e)
+        raise e
